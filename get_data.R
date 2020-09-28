5d3c8045a0b86410f6d19b846cc72d0b91d4e597
Niccolò <niccolo.salvini27@gmail.com>
2020-09-29 01:06:29 +0200

    few modifications

--- get_data.R
+++ get_data.R
@@ -2,4 +2,6 @@ source(here::here('REST_API/src/plumber.R'))
 
 links = all.links()
 d = scrape.all.info(links)
-write.csv(d, file = here::here("/shared-data/data.csv"))
+date = format(today(), "%d_%h_%Y")  %>% as.character()
+filename = paste0('imm_',date,'.csv')
+write.csv(d, file = here::here(paste0("/shared-data/",filename)))
