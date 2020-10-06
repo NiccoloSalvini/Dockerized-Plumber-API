6f623da9a567e42d22b8bb298f4065a5ca3cde6a
Niccolò <niccolo.salvini27@gmail.com>
2020-10-06 19:25:03 +0200

    placed vec.pacchetti in get_data

--- get_data.R
+++ get_data.R
@@ -1,10 +1,25 @@
 source(here::here("scraping/_links.R"),echo = FALSE)
 source(here::here("scraping/_complete.R"),echo = FALSE)
 
+vec.pacchetti = c("dplyr",
+                  "tibble",
+                  "magrittr",
+                  "rvest",
+                  "tidyr",
+                  "httr",
+                  "stringi",
+                  "lubridate",
+                  "jsonlite",
+                  "doParallel",
+                  "stringr",
+                  "here",
+                  "purrr")
+
+
 links = all.links()
 date = format(lubridate::today(), "%d-%b-%Y")  %>% as.character()
 filename = paste0('shared-data/imm-',date,'.csv')
 all = scrape.all.info(links = links)
 
 cat("writing .csv file...")
-write.csv(x = all, file = filename, append = T)  
+write.csv(x = all, file = here::here(filename), append = T)  
